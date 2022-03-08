ab03151cf716d5e243f4084444f46e6ee28c4523
Pvag <paolondon@gmail.com>
2022-03-08 21:36:29 +0000

    refactor: popolamento automatico list

--- app.js
+++ app.js
@@ -3,19 +3,49 @@ const numeriDOM = document.querySelector('.numeri')
 const oggettiDOM = document.querySelector('.oggetti')
 const listDOM = document.querySelector('.list')
 
-function appendTo(DOMelement) {
-  const element = listDOM.firstElementChild;
-  DOMelement.appendChild(element);
+const DOMelements = [
+  coloriDOM,
+  numeriDOM,
+  oggettiDOM
+]
+
+function appendFirstItemTo(DOMelement) {
+  const listElement = listDOM.firstElementChild;
+  DOMelement.appendChild(listElement);
 }
 
-coloriDOM.addEventListener('click', () => {
-  appendTo(coloriDOM);
-})
+DOMelements.forEach(
+  element => element.addEventListener('click', () => {
+    appendFirstItemTo(element)
+  })
+)
 
-numeriDOM.addEventListener('click', () => {
-  appendTo(numeriDOM);
-})
+const items = {
+  colori: ['Blu', 'Rosso', 'Giallo', 'Verde', 'Viola'],
+  numeri: [3124, 845, 524, 24, 435, 98, 249, 1390, 3099, 58],
+  oggetti: ['Casa', 'Chitarra', 'Computer', 'Sedia']
+}
 
-oggettiDOM.addEventListener('click', () => {
-  appendTo(oggettiDOM);
-})
+items.numeri.forEach(
+  numero => {
+    const numeroLi = document.createElement('li')
+    numeroLi.innerText = numero
+    listDOM.appendChild(numeroLi)
+  }
+)
+
+items.colori.forEach(
+  colore => {
+    const coloreLi = document.createElement('li')
+    coloreLi.innerText = colore
+    listDOM.appendChild(coloreLi)
+  }
+)
+
+items.oggetti.forEach(
+  oggetto => {
+    const oggettoLi = document.createElement('li')
+    oggettoLi.innerText = oggetto
+    listDOM.appendChild(oggettoLi)
+  }
+)
